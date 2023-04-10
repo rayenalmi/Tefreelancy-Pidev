13fff7784ae11208e56be5cb32b86bc500b6a84a
YassineMnasri223 <yassine.mnasri@esprit.tn>
2023-04-11 00:14:33 +0100

    first commit

---
+++ src/Form/BadgeType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Badge;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BadgeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('type')
+            ->add('image')
+            ->add('idTest')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Badge::class,
+        ]);
+    }
+}
---
+++ src/Form/QuestionType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Question;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuestionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('quest')
+            ->add('choice1')
+            ->add('choice2')
+            ->add('choice3')
+            ->add('response')
+            ->add('idTest')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Question::class,
+        ]);
+    }
+}
