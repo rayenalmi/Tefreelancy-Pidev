71bf10c437b6639c68baf5383ac52ad0556cb41f
rayenalmi <almi.rayyen@gmail.com>
2023-04-12 08:50:50 +0200

    formation and chapters controller

---
+++ src/Controller/FormationController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Formation;
+use App\Entity\Chapters;
+
+use App\Form\FormationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/formation')]
+class FormationController extends AbstractController
+{
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/', name: 'app_formation_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $formations = $entityManager
+            ->getRepository(Formation::class)
+            ->findAll();
+        $chapters = $entityManager
+            ->getRepository(Chapters::class)
+            ->findAll();
+
+        return $this->render('formation/index.html.twig', [
+            'formations' => $formations,
+            'chapters' => $chapters,
+        ]);
+    }
+
+    public function getFormationByName(string $name): array
+    {
+        $query = $this->entityManager->createQuery(
+            'SELECT u FROM App\Entity\Formation u WHERE u.name = :name'
+        )->setParameter('name', $name);
+
+        return $query->getResult();
+    }
+
+    #[Route('/new', name: 'app_formation_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $formation = new Formation();
+        $form = $this->createForm(FormationType::class, $formation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $u = $this->getFormationByName($form["name"]->getData());
+            if (count($u)!=0) 
+            {
+                return $this->redirectToRoute('app_formation_new', [], Response::HTTP_SEE_OTHER);
+            }
+
+            $entityManager->persist($formation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_formation_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('formation/new.html.twig', [
+            'formation' => $formation,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{idFormation}', name: 'app_formation_show', methods: ['GET'])]
+    public function show(Formation $formation): Response
+    {
+        return $this->render('formation/show.html.twig', [
+            'formation' => $formation,
+        ]);
+    }
+
+    #[Route('/{idFormation}/edit', name: 'app_formation_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Formation $formation, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(FormationType::class, $formation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_formation_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('formation/edit.html.twig', [
+            'formation' => $formation,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{idFormation}', name: 'app_formation_delete', methods: ['POST'])]
+    public function delete(Request $request, Formation $formation, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$formation->getIdFormation(), $request->request->get('_token'))) {
+            $entityManager->remove($formation);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_formation_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
