fbf65e8c184dad65c08f99462ac1539264dc638e
amenibensaada <amenisaada19@gmail.com>
2023-04-15 02:43:13 +0100

    change

--- src/Controller/CommunityController.php
+++ src/Controller/CommunityController.php
@@ -7,10 +7,14 @@ use App\Entity\Grouppost;
 
 use App\Form\CommunityType;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Bundle\DoctrineBundle\Controller\ControllerTrait;
+use Doctrine\Persistence\ManagerRegistry;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 #[Route('/community')]
 class CommunityController extends AbstractController
@@ -51,6 +55,59 @@ class CommunityController extends AbstractController
             'community' => $community,
             'form' => $form,
         ]);
+    }
+    #[Route('/search-groups', name: 'search_groups',methods: ['GET'])]
+    public function searchGroupsByName(
+        Request $request,
+        ManagerRegistry $doctrine
+    ) {
+        dump('helloooooo');
+
+        $name = $request->query->get('name');
+
+        $queryBuilder = $doctrine
+            ->getManager()
+            ->createQueryBuilder()
+            ->select('g')
+            ->from(Community::class, 'g')
+            ->where('g.name LIKE :name')
+            ->setParameter('name', '%' . $name . '%');
+
+        $groups = $queryBuilder->getQuery()->getResult();
+
+        // Convert the groups to an array of associative arrays for easier serialization
+        $data = array_map(function ($group) {
+            return [
+                'id' => $group->getIdCommunity(),
+                'name' => $group->getName(),
+                // Add any other properties you want to include in the JSON response
+            ];
+        }, $groups);
+        dump('heloooooooooooooooo testtttttttttt');
+
+        return new JsonResponse($data);
+    }
+
+    #[Route('/sort-groups', name: 'sort_groups',methods: ['GET'])]
+
+    public function sortGroupsByName(
+        Request $request,
+        ManagerRegistry $doctrine
+    ) {
+        $groups = $this->getDoctrine()
+            ->getRepository(Community::class)
+            ->findBy([], ['name' => 'ASC']);
+
+        // Convert the groups to an array of associative arrays for easier serialization
+        $data = array_map(function ($group) {
+            return [
+                'id' => $group->getId(),
+                'name' => $group->getName(),
+                // Add any other properties you want to include in the JSON response
+            ];
+        }, $groups);
+
+        return new JsonResponse($data);
     }
 
     #[Route('/{idCommunity}', name: 'app_community_show', methods: ['GET'])]
